c553f3e24dd5e3796bf09a395be1db9fc0db1dd5
Ariel Alonso <aalonso@ipcoop.com>
2014-06-28 22:48:43 -0400

    Add BaseRank class with default implementation

--- src/HighCard.groovy
+++ src/HighCard.groovy
@@ -1,24 +1,4 @@
-class HighCard implements Rank {
-  boolean evaluate(Hand hand) {
-    return true
-  }
-
-  Hand resolveTie(Hand a, Hand b) {
-    a.sort()
-    b.sort()
-
-    a.cards = a.cards.reverse()
-    b.cards = b.cards.reverse()
-
-    while (a.size() > 0) {
-      def c1 = a.cards.pop()
-      def c2 = b.cards.pop()
-
-      if (Card.val(c1.value) < Card.val(c2.value)) return a
-      if (Card.val(c1.value) > Card.val(c2.value)) return b
-    }
-  }
-
+class HighCard extends BaseRank {
   String toString() {
     "High Card"
   }
--- src/Pair.groovy
+++ src/Pair.groovy
@@ -1,8 +1,11 @@
-class Pair implements Rank {
+class Pair extends BaseRank {
+
+  @Override
   boolean evaluate(Hand hand) {
     hand.cards.groupBy { c -> c.value }.count { k,v -> v.size() == 2} == 1
   }
 
+  @Override
   Hand resolveTie(Hand a, Hand b) {
     def p1 = a.cards.groupBy { it.value }.find { k,v -> v.size() == 2 }
     def p2 = b.cards.groupBy { it.value }.find { k,v -> v.size() == 2 }
@@ -10,13 +13,7 @@ class Pair implements Rank {
     if (Card.val(p1.key) < Card.val(p2.key)) return a
     if (Card.val(p1.key) > Card.val(p2.key)) return b
 
-    while (a.size() > 0) {
-      def c1 = a.cards.pop()
-      def c2 = b.cards.pop()
-
-      if (Card.val(c1.value) < Card.val(c2.value)) return a
-      if (Card.val(c1.value) > Card.val(c2.value)) return b
-    }
+    super.resolveTie(a,b)
   }
 
   public String toString() {
