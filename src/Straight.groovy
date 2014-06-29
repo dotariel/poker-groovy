a50585791a24b62b79a57b6f3c30bfb6fb0ee376
Ariel Alonso <aalonso@ipcoop.com>
2014-06-29 19:50:36 -0400

    Add tie resolution for Straight

--- src/Straight.groovy
+++ src/Straight.groovy
@@ -1,6 +1,6 @@
 class Straight extends BaseRank {
   Hand compare(Hand a, Hand b) {
-    throw new NotImplemented()
+    super.compare(a,b)
   }
 
   String toString() {
--- test/unit/StraightTest.groovy
+++ test/unit/StraightTest.groovy
@@ -3,21 +3,11 @@ import org.junit.*
 @Mixin(RankTestMixin)
 class StraightTest {
 
+  def rank = new Straight()
+
   @Test
   public void should_set_rank_on_visit() {
     checkVisit(Hand.mockStraight(), Straight)
-  }
-
-  @Ignore
-  @Test
-  public void should_return_rank() {
-    def rank = new Straight()
-
-    assert rank.evaluate(Hand.mockStraight()) == true
-    assert rank.evaluate(Hand.mockThreeOfAKind()) == false
-    assert rank.evaluate(Hand.mockTwoPair()) == false
-    assert rank.evaluate(Hand.mockPair()) == false
-    assert rank.evaluate(Hand.mockHighCard()) == false
   }
 
   @Test
@@ -27,4 +17,17 @@ class StraightTest {
     assert rank.isConsecutive([1,2,3,4,6]) == false
     assert rank.isConsecutive([1,2,3,4,5]) == true
   }
+
+  @Test
+  public void should_resolve_tie() {
+    def a,b
+
+    a = new Hand(['4S', '5H', '6D', '7C', '8C'])
+    b = new Hand(['5D', '6C', '7S', '8D', '9C'])
+    checkWinner(rank,b,a)
+
+    a = new Hand(['4S', '5H', '6D', '7C', '8C'])
+    b = new Hand(['4C', '5D', '6H', '7H', '8S'])
+    checkTie(rank,a,b)
+  }  
 }
