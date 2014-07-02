1b8a5fa96c32abd208b76cb80eea36785af0bfe7
Ariel Alonso <aalonso@ipcoop.com>
2014-07-01 21:42:07 -0400

    Modified console output

--- src/Poker.groovy
+++ src/Poker.groovy
@@ -7,9 +7,9 @@ class Poker {
     println ""
     println "Instructions:"
     println " - Enter each hand in the following format:"
-    println "    {F}{C} {F}{C} {F}{C} {F}{C} {F}{C}"
+    println "    {V}{S} {V}{S} {V}{S} {V}{S} {V}{S}"
     println "     Suits: C=Clubs, H=Hearts, S=Spades, D=Diamonds"
-    println "     Faces: 2,3,4,5,6,7,8,9,T,J,Q,K,A"
+    println "     Values: 2,3,4,5,6,7,8,9,T,J,Q,K,A"
     println "  - Example:"
     println "     AC 2D TH 3S 5D"
     println ""
@@ -21,7 +21,7 @@ class Poker {
       Hand h1 = new Hand(a.tokenize(' '))
       Hand h2 = new Hand(b.tokenize(' '))
 
-      println evaluator.choose(h1,h2) ?: "Tie" 
+      println "Winner: " + (evaluator.choose(h1,h2) ?: "None") 
     }
     catch (InvalidHand e) {
       println "Hands must consist of 5 cards."
